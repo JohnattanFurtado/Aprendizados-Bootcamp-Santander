5668116114ee1c08f0efc1c0a1b8b168cd8adf07
JohnattanFurtado <johfurtado@gmail.com>
2022-07-13 09:43:10 -0300

    mais anotacoes javasc

---
+++ JavaScript/modulo/main..mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade('John', 'Floripa'));
+console.log(mostraHobby('John', 'Jogar Rugby'));
+console.log(mostraIdade('John', 28));
